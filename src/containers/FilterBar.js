afea4d4fcbf44306194d40e4eb66b4223e257aab
Mallory Woods <malvwoods@gmail.com>
2019-07-02 17:46:20 -0400

    some button styling

--- src/containers/FilterBar.js
+++ src/containers/FilterBar.js
@@ -5,17 +5,17 @@ import { filterPosts, fetchNewArticles } from '../actions/posts';
 
 const FilterBar = ({filterPosts, fetchNewArticles}) => {
   return (
-    <div className={"page-header"}>
+    <div className='page-header'>
       <Header as='h3' icon textAlign='center'>
           <Header.Content>SYMPOSIA</Header.Content>
          <Icon name='star outline' circular />
          <Header.Content>Filter By Topic</Header.Content>
          <br/>
-         <button className='button' onClick={() => filterPosts("IMMIGRATION")}>IMMIGRATION</button>
-         <button className='button' onClick={() => filterPosts("NATIONAL+DEBT")}>NATIONAL DEBT</button>
-         <button className='button' onClick={() => filterPosts("2020")}>2020 ELECTION</button>
-         <button className='button' onClick={() => filterPosts("ABORTION")}>ABORTION</button>
-         <button className='button' onClick={() => filterPosts("CLIMATE")}>CLIMATE</button>
+         <button className='topic-button' onClick={() => filterPosts("IMMIGRATION")}>IMMIGRATION</button>
+         <button className='topic-button' onClick={() => filterPosts("NATIONAL+DEBT")}>NATIONAL DEBT</button>
+         <button className='topic-button' onClick={() => filterPosts("2020")}>2020 ELECTION</button>
+         <button className='topic-button' onClick={() => filterPosts("ABORTION")}>ABORTION</button>
+         <button className='topic-button' onClick={() => filterPosts("CLIMATE")}>CLIMATE</button>
        </Header>
        {/*<Header as="h4" textAlign='center'>
         <Header.Content>
